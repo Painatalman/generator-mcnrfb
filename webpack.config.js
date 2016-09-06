d3c5bb59df8eabc897de0ed08670d71dae99ff57
Painatalman <maiquedi@gmail.com>
2016-09-06 03:31:26 +0100

    :fire: first batch of files

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+      contentBase: './dist'
+  },
+  configFile: './.eslintrc',
+  devtool: 'source-map',
+  plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        include: [
+          path.resolve(__dirname, 'scripts')
+        ]
+      },{
+        test: /\.css$/,
+        loader: 'style-loader!css-loader!postcss-loader',
+        include: path.join(__dirname, 'src')
+    }
+    ]
+  }
+};
